0a44a1514cb20e3a1f735809fbeb05612597ad2b
Wiris Rafael Januario Wernek <rafaeljwerneck43@gmail.com>
2023-05-19 13:46:45 -0300

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAFz4MJNgcLSbNYz_BcpPyRzuX12rRzWpQ",
+  authDomain: "thenews-1d5b6.firebaseapp.com",
+  projectId: "thenews-1d5b6",
+  storageBucket: "thenews-1d5b6.appspot.com",
+  messagingSenderId: "469353727431",
+  appId: "1:469353727431:web:e06ded8b4b6de72fcd45e2",
+  measurementId: "G-6M8Z8JDNBM"
+};
+
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+export const FIREBASE_DB = getFirestore(FIREBASE_APP);
+
+const analytics = getAnalytics(FIREBASE_APP);
